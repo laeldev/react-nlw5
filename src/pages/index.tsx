317b1239d79d3ccb2ebc4f770323120a44b394ad
Andre Lael <andre.lael@outlook.com>
2021-04-25 18:42:58 -0300

    Aula 2 Concluida Criado o player e conexao API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,62 @@
-import { Header } from "../components/Header";
+/* 
+SPA
+
+useEffect(() =>{
+  fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+},[])
+
+ */
+
+/*
+SSR
+export async function getServerSideProps(){
+
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props:{
+      episodes: data,
+    }
+  }
+}
+
+*/
+/*
+SSG
+export async function getStaticProps(){
+
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props:{
+      episodes: data,
+    },
+    revalidate: 60*60*8,
+  }
+}
+*/
 
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
   return (
-    <Header />
+      <h1>index</h1>
   );
+}
+
+export async function getStaticProps(){
+
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props:{
+      episodes: data,
+    },
+    revalidate: 60*60*8,
+  }
 }
